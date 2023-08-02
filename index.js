a816a39263552fbd323a54883fc9a1dee4bd2567
RajobAli <freelancerrajobali@gmail.com>
2023-08-02 08:05:09 +0600

    added footer

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-
-
-var marks = 110;
-
-if(marks<0 || marks>100){
-    var a = "Invalid marks";
-  
-}
-else if(marks
-    >=80){
-    a = "A+";
+var m = document.querySelector(".one");
+var button = document.querySelector(".btn");
+button.addEventListener("click",function(){
+    var marks = m.value;
+    if(marks<0 || marks>100){
+        var a = "Invalid marks";
+      
+    }
+    else if(marks
+        >=80){
+        a = "A+";
+        
+    }
+    else if(marks>=70){
+        a = "A";
+       
+    }
+    else if(marks>=60){
+        a = "B";
     
-}
-else if(marks>=70){
-    a = "A";
-   
-}
-else if(marks>=60){
-    a = "B";
+    }
+    else if(marks>=50){
+        a = "C";
+    
+    }
+    else{
+        a = "Fail";
+    
+    
+    }
+    document.write("Your grade is"  +   " :  " +a);
+  
+    
+});
 
-}
-else if(marks>=50){
-    a = "C";
 
-}
-else{
-    a = "Fail";
-
-
-}
-document.write("Your grade is"  +   " :  " +a);
